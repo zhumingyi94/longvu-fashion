9394d410aefb29b853bc6b398a0e1dceacd591e2
zhumingyi94 <dominhnhat2004@gmail.com>
2024-08-21 12:27:56 +0700

    first commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
+      <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet" />
       <Head />
       <body>
         <Main />
